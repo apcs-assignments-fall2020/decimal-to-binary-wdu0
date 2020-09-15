e083c3af8fae83cfd5aced41739001a3c2d8ba91
wdu0 <23wdu@athenian.org>
2020-09-15 07:20:23 +0000

    Added some code to the methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,28 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int bi_int = Integer.parseInt(binary);
+        int i = 0;
+        int ending = 0;
+        int result = 0;
+
+        while (bi_int > 0) {
+            ending = bi_int % 10;
+            ending *= Math.pow(2, i);
+            bi_int /= 10;
+            result += ending;
+            i++;
+        }
+        return result;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format: ");
+        String input = scan.next();
+
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(input));
+        scan.close();
     }
 }
